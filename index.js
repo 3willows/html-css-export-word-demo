7cb766361c97910820642a856955365d89d24024
Jasper Wong <wlcjasper@gmail.com>
2024-05-14 19:10:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import {htmlCssExportWord} from "html-css-export-word"
+
+const htmlInput = document.getElementById("htmlInput")
+const cssInput = document.getElementById("cssInput")
+const result = document.getElementById("result").contentWindow.document
+
+const button = document.querySelector("button")
+
+function updateResult() {
+  result.open()
+  result.write(
+    `<html><head><style>${cssInput.value}</style></head><body>${htmlInput.value}</body></html>`
+  )
+  result.close()
+}
+
+htmlInput.addEventListener("input", updateResult)
+cssInput.addEventListener("input", updateResult)
+
+button.addEventListener("click", ()=>{
+  console.log("hihi")
+  htmlCssExportWord(htmlInput.value, cssInput.value)
+})
+window.onload = updateResult()
